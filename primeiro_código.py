c84c2bbf26c500f6ee4ef2b997edb966aa74eebf
Luanna Priscila <luannasilva7765@gmail.com>
2026-03-11 17:32:14 -0300

    Adiciona primeiros exercícios de Python: print, variáveis e input

---
+++ primeiro_código.py
@@ -0,0 +1,29 @@
+# Primeiro contato com Python
+# Estudos iniciais de programação
+
+#entendendo como funciona o print, noção de mensagem
+print('Olá,Mundo!')
+print(7+4)
+print('7'+'4')
+
+#aula sobre variáveis
+nome='Luanna'
+idade=18
+peso=68.2
+print(nome,idade,peso)
+
+#interatividades
+nome=input('Qual é o seu nome?')
+idade=input('Quantos anos você tem?')
+peso=input('Qual seu peso?')
+print('Seu nome é',nome,'.Sua idade é',idade,'.Seu peso é',peso)
+
+#exercício:aula1
+nome=input('Me diga seu nome?')
+print('Bem vinda',nome,'é um prazer ter você conosco!')
+
+#exercício 2:aula 1
+dia=input('Que dia você nasceu?')
+mes=input('Qual mês você nasceu?')
+ano=input('Qual ano você nasceu?')
+print('Você nasceu no dia',dia,'do mês',mes,'do ano de',ano)
